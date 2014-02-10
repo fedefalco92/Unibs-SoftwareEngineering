bde3daa49cc566b0b4088b7be3ed99ee742c10b6
fedefalco92 <federicofalcone92@msn.com>
2014-02-10 13:20:00 +0100

    It Works

--- Software Engineering - Version 1/src/MainClass.java
+++ Software Engineering - Version 1/src/MainClass.java
@@ -6,8 +6,7 @@ public class MainClass {
 		System.out.println("Nel mezzo del cammin di nostra vita");
 		System.out.println("Mi ritrovai in una selva oscura");
 		TestClass.stampaNumero(10);
-		
-		
+		System.out.println("It works");
 	}
 
 }
