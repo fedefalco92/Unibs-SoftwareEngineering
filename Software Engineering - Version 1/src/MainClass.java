d275f3ab4b2ae8fc19c9cebbbe6cabf57bf4cd24
fedefalco92 <federicofalcone92@msn.com>
2014-02-10 13:21:08 +0100

    Removin Test Class

--- Software Engineering - Version 1/src/MainClass.java
+++ Software Engineering - Version 1/src/MainClass.java
@@ -5,7 +5,6 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("Nel mezzo del cammin di nostra vita");
 		System.out.println("Mi ritrovai in una selva oscura");
-		TestClass.stampaNumero(10);
 		System.out.println("It works");
 	}
 
--- Software Engineering - Version 1/src/TestClass.java
+++
@@ -1,7 +0,0 @@
-
-public class TestClass {
-	
-	public static void stampaNumero(int numero){
-		System.out.printf("%d\n", numero);
-	}
-}
