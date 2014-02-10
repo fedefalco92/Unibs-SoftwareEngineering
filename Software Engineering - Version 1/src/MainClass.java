c13ba410d801722ce8a296070fe20b155d518aef
fedefalco92 <federicofalcone92@msn.com>
2014-02-10 13:05:46 +0100

    Update

--- Software Engineering - Version 1/src/MainClass.java
+++ Software Engineering - Version 1/src/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("Nel mezzo del cammin di nostra vita");
 		System.out.println("Mi ritrovai in una selva oscura");
+		TestClass.stampaNumero(10);
 		
 	}
 
