aac191b21a7162c6e3236a0205a8aaa619629404
fedefalco92 <federicofalcone92@msn.com>
2014-02-10 14:26:31 +0100

    Removed comment.

--- Software Engineering - Version 1/src/MainClass.java
+++ Software Engineering - Version 1/src/MainClass.java
@@ -2,7 +2,6 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Nel mezzo del cammin di nostra vita");
 		System.out.println("Mi ritrovai in una selva oscura");
 		System.out.println("It works");
