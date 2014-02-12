7dc56500a8f8e635e77111b11e23a703ef42d236
massi92 <maxcpp@hotmail.it>
2014-02-12 11:27:13 +0100

    Aggiunta di una riga

--- Software Engineering - Version 1/src/MainClass.java
+++ Software Engineering - Version 1/src/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 		System.out.println("Nel mezzo del cammin di nostra vita");
 		System.out.println("Mi ritrovai in una selva oscura");
 		System.out.println("It works");
+		System.out.println("Ciao");
 	}
 
 }
