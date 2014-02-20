ad4017d010994fc36514d6d489a6b550708d40b1
fedefalco92 <federicofalcone92@msn.com>
2014-02-20 15:39:11 +0100

    Modifica Azione

--- Software Engineering - Version 1/src/Azione.java
+++ Software Engineering - Version 1/src/Azione.java
@@ -1,4 +1,21 @@
 
-public class Azione {
+public class Azione extends Elemento{
 
+		private String nome;
+		private Elemento ingresso;
+		private Elemento uscita;
+		
+		public Azione (String nome, Elemento ingresso, Elemento uscita){
+			this.nome = nome;
+			this.ingresso = ingresso;
+			this.uscita = uscita;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuffer output=new StringBuffer();
+			
+			output.appen("Nome: " + nome + "\n");
+		return super.toString();
+		}
 }
