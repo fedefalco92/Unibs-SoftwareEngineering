4ba0c4985e9a090e00464d717d32ea963e92dcd9
Maff3x <maff3x@yahoo.it>
2014-02-20 16:08:23 +0100

    Aggiunta End

--- Software Engineering - Version 1/src/Modello.java
+++ Software Engineering - Version 1/src/Modello.java
@@ -1,7 +1,15 @@
+import java.util.Vector;
+
 
 public class Modello {
 	
 	Start start;
+	Vector<Azione> azioni;
+	Vector<Branch> branch;
+	Vector<Merge> merge;
+	Vector<Fork> fork;
+	Vector<Join> join;
+	End end;
 	
 
 }
