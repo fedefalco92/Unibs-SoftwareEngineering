03d65c18bdea23e9dbaab4a5f4d7c54f6f3a4ded
Maff3x <maff3x@yahoo.it>
2014-02-20 16:16:44 +0100

    modifica modello

--- Software Engineering - Version 1/src/Modello.java
+++ Software Engineering - Version 1/src/Modello.java
@@ -3,13 +3,50 @@ import java.util.Vector;
 
 public class Modello {
 	
-	Start start;
-	Vector<Azione> azioni;
-	Vector<Branch> branch;
-	Vector<Merge> merge;
-	Vector<Fork> fork;
-	Vector<Join> join;
-	End end;
+	private Start start;
+	private Vector<Azione> azioni;
+	private Vector<Branch> branch;
+	private Vector<Merge> merge;
+	private Vector<Fork> fork;
+	private Vector<Join> join;
+	private End end;
 	
+	public Modello(){
+		start = new Start();
+		azioni = new Vector<Azione>();
+		branch = new Vector<Branch>();
+		merge = new Vector<Merge>();
+		fork = new Vector<Fork>();
+		join = new Vector<Join>();
+		end = new End();
+	}
+	
+	public void setStart(Start _start){
+		start = _start;
+	}
+	
+	public void setEnd(End _end){
+		end = _end;
+	}
+	
+	public void aggiungiAzione(Azione azione){
+		azioni.add(azione);
+	}
+	
+	public void aggiungiBranch(Branch _branch){
+		branch.add(_branch);
+	}
+	
+	public void aggiungiMerge(Merge _merge){
+		merge.add(_merge);
+	}
+	
+	public void aggiungiFork(Fork _fork){
+		fork.add(_fork);
+	}
+	
+	public void aggiungiJoin(Join _join){
+		join.add(_join);
+	}
 
 }
