5f440a9ed3a0721a7ab4a08cc830c3d06c29d28a
fedefalco92 <federicofalcone92@msn.com>
2014-02-22 00:39:35 +0100

    Modifica costruzione modello

--- Software Engineering - Version 1/src/CreazioneModello.java
+++ Software Engineering - Version 1/src/CreazioneModello.java
@@ -1,22 +1,27 @@
 /*VERSIONE 1: NO FORK E JOIN
  * 
- * questa classe conterrà un menu per gestire la creazione di un nuovo modello
- * permetterà di
+ * questa classe conterra' un menu per gestire la creazione di un nuovo modello
+ * permettera' di
  * -iniziare la creazione
- * -->ci vorrà un sottomenu per scegliere cosa aggiungere..
+ * -->ci vorra' un sottomenu per scegliere cosa aggiungere..
  * -terminare la creazione e quindi salvare il modello
  * --come testo
  * --come oggetto
- * in teoria bisognerebbe poter terminare solo se il modello è corretto..
+ * in teoria bisognerebbe poter terminare solo se il modello e' corretto..
  * 
  * 
  */
 public class CreazioneModello {
 	
-	private Modello modello;
+	private static Modello modello;
 	
+	public static void creaModello(String nomeModello){
+		modello = new Modello(nomeModello);
+	}
+	
+	/*
 	public CreazioneModello(String nomemodello){
 		modello = new Modello(nomemodello);
 	}
-	
+	*/
 }
