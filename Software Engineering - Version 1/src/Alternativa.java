8e0780eedefa3996e2fbea816e715bfe94fad954
Maff3x <maff3x@yahoo.it>
2014-02-27 12:02:55 +0100

    iniziate classi Alternativa e Flusso

--- Software Engineering - Version 1/src/Alternativa.java
+++ Software Engineering - Version 1/src/Alternativa.java
@@ -1,4 +1,40 @@
+/**
+ * Classe da usare come alternativa di un branch...
+ * &egrave come se fosse un modello a se'...
+ * -inizio: al posto del nodo iniziale ha un branch 
+ * -fine: pu&ograve terminare su un merge o sul nodo finale.
+ * 
+ * @author Maffi
+ *
+ */
+public class Alternativa extends Modello {
+	
+	
+	private Branch padre; //per ora uso il nome padre, si potrebbe usare anche ingresso o inizio 
+	private Elemento uscita;
+	
+	public Alternativa(String nome, Branch _padre) {
+		super(nome);
+	this.setPadre(_padre);
+	}
 
-public class Alternativa {
+	public Branch getPadre() {
+		return padre;
+	}
+
+	public void setPadre(Branch padre) {
+		this.padre = padre;
+	}
+
+	public Elemento getUscita() {
+		return uscita;
+	}
+
+	public void setUscita(Elemento uscita) {
+		this.uscita = uscita;
+	}
+	
+	
+	
 
 }
