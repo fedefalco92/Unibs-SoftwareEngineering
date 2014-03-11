933ac8eba921933cb36b13280313e54d7a79b60a
fedefalco92 <federicofalcone92@msn.com>
2014-03-11 12:06:17 +0100

    Pulizia cose inutili

--- Software Engineering - Version 1/src/Join.java
+++ Software Engineering - Version 1/src/Join.java
@@ -3,22 +3,14 @@ import java.util.Vector;
 
 public class Join extends Elemento implements ElementoTerminale {
 
-	//private Vector<Flusso> flussiIN;
 	private Vector<Elemento> ingressi;
 	private Fork forkAssociato;
 	private Elemento uscita;
-	//private boolean incompleto;
 	
-	/*
-	 * nel costruttore non metto il fork padre dato che 
-	 * il join viene creato prima del fork padre e viene passato
-	 * in ingresso al fork padre come join terminale
-	 */
 	public Join(String nome) {
 		super("JOIN", nome);
-		//flussiIN = new Vector <Flusso>();
 		ingressi = new Vector <Elemento>();
-		//incompleto = true;
+		uscita = null;
 	}
 
 	public Fork getForkAssociato() {
@@ -38,14 +30,6 @@ public class Join extends Elemento implements ElementoTerminale {
 		this.uscita = uscita;
 	}
 	
-	/*
-	public void aggiungiFlussoIN(Flusso f){
-		flussiIN.add(f);
-		if(flussiIN.size()>=2)
-			incompleto=false;
-	}
-	*/
-	
 	@Override
 	public String toString() {
 		StringBuffer output=new StringBuffer();
@@ -60,29 +44,18 @@ public class Join extends Elemento implements ElementoTerminale {
 				output.append(", ");
 			}
 			output.append(ingressi.lastElement().getElementoString());
-			
-//			if(incompleto)
-//				output.append("incompleto");
 		}
-		/*
-		 * if(!flussiIN.isEmpty()){
-			
-			for(int i = 0; i < flussiIN.size() - 1; i++){
-				output.append(flussiIN.get(i).getFlussoString());
-				output.append(", ");
-			}
-			output.append(flussiIN.lastElement().getFlussoString());
-			
-			if(incompleto)
-				output.append("incompleto");
-		}
-		 */
-		else output.append("empty");
+		
+		else 
+			output.append("empty");
+		
 		output.append(") - out(");
-		if(uscita!=null) output.append(uscita.getElementoString());
-		else output.append("null");
+		if(uscita!=null) 
+			output.append(uscita.getElementoString());
+		else 
+			output.append("null");
+		
 		output.append(")");
-
 		return output.toString();
 	}
 
